220d536eae19f9b0c881e6325282d75e616fc61f
Mark8495 <markbyrne8495@gmail.com>
2022-06-08 13:28:04 +0000

    changes to requirements

--- run.py
+++ run.py
@@ -124,3 +124,116 @@ while start_game == True:
         print('Normal, where you only guess one word!')
         print('Hard, where you have to guess two words!')
         print('Finally, Super Hard, where you have to guess four words!')
+    # sets the diffculty for the player
+    challenge = input(
+        "Press 1 for easy mode, 2 for hard mode and 3 for super hard mode: \n")
+    # sets the max attempts for a player
+    max_attempts = num_of_attempts(challenge)
+    count = max_attempts + 1
+    play_game = True
+    while play_game == True:
+        # sets prettytable to default
+        table.clear()
+        if max_attempts == 0:
+            # what happens when you lose the game
+            Lose = pyfiglet.figlet_format("You Lose!", justify="center")
+            print(Lose)
+            restart_game()
+        else:
+            remain = count - max_attempts
+            # cheatsheet
+            print(answer1, answer2, answer3, answer4)
+            # input for guess
+            word = input('Guess a five letter Word: ').lower()
+            if len(word) != WORD_LENGTH:
+                # if the input does not work for the game
+                print('You guess must have five letters')
+                complete_guess = False
+            else:
+                # starts the logic for the game.
+                complete_guess = True
+                cls()
+            # starts the game
+            while complete_guess == True:
+                if challenge == '2':
+                    # if the game is in hard mode
+                    if guess_1 == 1:
+                        # if the word has been guessed it will fill the rest of the array.
+                        attempts1.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        attempts1.append(guess_match(answer1, word))
+                    if word == answer1:
+                        # removes word from the options
+                        guess_1 = 1
+                    if guess_2 == 1:
+                        attempts2.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        # joins the word into the array
+                        attempts2.append(guess_match(answer2, word))
+                    if word == answer2:
+                        guess_2 = 1
+                    if (guess_1 + guess_2) == 2:
+                        win_message(remain)
+                        guess_1 = 0
+                        guess_2 = 0
+                        max_attempts = 0
+                        restart_game()
+                    else:
+                        table.add_column("Hard", attempts1)
+                        table.add_column('Mode', attempts2)
+                        print(table)
+                    complete_guess = False
+                elif challenge == '3':
+                    # if the game is in super hard mode
+                    if guess_1 == 1:
+                        attempts1.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        attempts1.append(guess_match(answer1, word))
+                    if word == answer1:
+                        guess_1 = 1
+                    if guess_2 == 1:
+                        attempts2.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        attempts2.append(guess_match(answer2, word))
+                    if word == answer2:
+                        guess_2 = 1
+                    if guess_3 == 1:
+                        attempts3.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        attempts3.append(guess_match(answer3, word))
+                    if word == answer3:
+                        guess_3 = 1
+                    if guess_4 == 1:
+                        attempts4.append(" ".join(["_"] * WORD_LENGTH))
+                    else:
+                        attempts4.append(guess_match(answer4, word))
+                    if word == answer4:
+                        guess_4 = 1
+
+                    if (guess_1 + guess_2 + guess_3 + guess_4) == 4:
+
+                        win_message(remain)
+                        guess_1 = 0
+                        guess_2 = 0
+                        guess_3 = 0
+                        guess_4 = 0
+                        max_attempts = 0
+                        restart_game()
+                    else:
+                        table.add_column("Wordle", attempts1)
+                        table.add_column('Super', attempts2)
+                        table.add_column("Hard", attempts3)
+                        table.add_column('Mode', attempts4)
+                        print(table)
+                    complete_guess = False
+                else:
+                    # if the game is in normal mode.
+                    attempts1.append(guess_match(answer1, word))
+                    table.add_column("Wordle", attempts1)
+                    print(table)
+                    if word == answer1:
+                        win_message(remain)
+                        max_attempts = 0
+                        restart_game()
+                    complete_guess = False
+                max_attempts -= 1
